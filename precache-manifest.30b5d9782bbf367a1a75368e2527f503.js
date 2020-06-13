585fe948e75131a4f91edfa7cfb24fb9b1b24308
Vera <overaaloe@gmail.com>
2020-06-13 23:11:56 +0800

    Updates

---
+++ precache-manifest.30b5d9782bbf367a1a75368e2527f503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee23a574c3bca13f493f149b1f55d35e",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "c9476b2f0d0160bab03c",
+    "url": "/TodoList/static/css/main.83b393ba.chunk.css"
+  },
+  {
+    "revision": "96724835e713d8700200",
+    "url": "/TodoList/static/js/2.b4503a98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.b4503a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9476b2f0d0160bab03c",
+    "url": "/TodoList/static/js/main.6af4de7a.chunk.js"
+  },
+  {
+    "revision": "ad94ee2b76f39401f7f6",
+    "url": "/TodoList/static/js/runtime-main.e0493b17.js"
+  }
+]);
